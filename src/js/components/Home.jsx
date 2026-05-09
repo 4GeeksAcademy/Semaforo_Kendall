1ec6901b75c61e73962e8947280053687f143efc
Kenube21 <kendallsh51@gmail.com>
2026-05-09 04:34:42 +0000

    details

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,21 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 
 //create your first component
 const Home = () => {
 	const [color, setColor] = useState("")
-	
+	const [isChanging, setIsChanging] = useState(false)
+
 	const colors = [
 		"red",
 		"yellow",
 		"green"
 	]
 
+	useEffect(() => {
 
-	function getColorBg (item) {
-		if (item == "red"){
+		let intervalo;
+		if (isChanging) {
+			intervalo = setInterval(() => {
+				setColor((prevColor) => {
+					// Buscamos el índice del color actual
+					const currentIndex = colors.indexOf(prevColor);
+					// Calculamos el siguiente índice (si es el último, vuelve al 0)
+					const nextIndex = (currentIndex + 1) % colors.length;
+					return colors[nextIndex];
+				});
+			}, 2000); // Cambia cada 2 segundos
+		}
+
+
+		return () => clearInterval(intervalo);
+	}, [isChanging]);
+
+
+
+	function getColorBg(item) {
+		if (item == "red") {
 			return " bg-danger "
-		} 
+		}
 		if (item == "yellow") {
 			return " bg-warning "
 		}
@@ -24,10 +45,10 @@ const Home = () => {
 		}
 	}
 
-	function isColorActive (item) {
-		if (item == "red" && item == color){
+	function isColorActive(item) {
+		if (item == "red" && item == color) {
 			return " turn-on-red "
-		} 
+		}
 		if (item == "yellow" && item == color) {
 			return " turn-on-yellow "
 		}
@@ -36,22 +57,35 @@ const Home = () => {
 		}
 		return ""
 	}
-	
-	
+
+
 	return (
 		<div className="d-flex flex-column align-items-center">
-			<div className="bg-dark p-2" style={{width: '5px', height: '40px'}}></div>
-            <div className="d-inline-flex bg-dark">
+			<div className="bg-dark p-2" style={{ width: '5px', height: '40px' }}></div>
+			<div className="d-inline-flex bg-dark rounded">
 				<div className="d-flex flex-column align-items-center gap-2 p-2 ">
 					{
-						colors.map((item)=> {
-							return <div key={item} 
-							className={ "col-4 rounded-4 p-3"+ getColorBg(item) + isColorActive(item) }
-							onClick={() => setColor(item)}
+						colors.map((item) => {
+							return <div key={item}
+								className={"rounded-circle p-5" + getColorBg(item) + isColorActive(item)}
+								onClick={() => {
+									item == color ? setColor("") : setColor(item)
+								}}
 							></div>
 						})
 					}
-					
+
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-12">
+					<button
+						className={`m-5 btn ${isChanging ? "btn-danger" : "btn-success"}`}
+						onClick={() => setIsChanging(!isChanging)}
+					>
+						{isChanging ? "Detener Auto" : "Iniciar Auto"}
+					</button>
+
 				</div>
 			</div>
 
