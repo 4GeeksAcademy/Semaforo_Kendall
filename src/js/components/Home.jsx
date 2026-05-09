ed738b4f634781f1a3eb56f867e158a88affec8a
Kenube21 <kendallsh51@gmail.com>
2026-05-09 02:38:50 +0000

    funciona

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [color, setColor] = useState("yellow")
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const colors = [
+		"red",
+		"yellow",
+		"green"
+	]
+
+	function getColorBg (item) {
+		if (item == "red"){
+			return "bg-danger"
+		} 
+		if (item == "yellow") {
+			return "bg-warning"
+		}
+		if (item == "green") {
+			return "bg-success"
+		}
+	}
+
+	function isColorActive (item) {
+		if (item == "red" && item == color){
+			return " turn-on-red"
+		} 
+		if (item == "yellow" && item == color) {
+			return " turn-on-yellow"
+		}
+		if (item == "green" && item == color) {
+			return " turn-on-green"
+		}
+		return ""
+	}
+	
+	
+	return (
+		<div className="d-flex flex-column align-items-center">
+			<div className="bg-dark p-2" style={{width: '5px', height: '40px'}}></div>
+            <div className="d-inline-flex bg-dark">
+				<div className="d-flex flex-column align-items-center gap-2 p-2 ">
+					{
+						colors.map((item)=> {
+							return <div key={item} 
+							className={ "col-4 rounded-4 p-3 "+ getColorBg(item) + isColorActive(item) }
+							onClick={() => setColor(item)}
+							></div>
+						})
+					}
+					
+				</div>
+			</div>
+
 		</div>
 	);
 };
